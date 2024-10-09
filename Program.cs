2eceda067ebc711b064e050000d5d43be0b7e517
Murat Abbaslı <allahverdi.abbasli.1995@gmail.com>
2024-10-09 14:47:19 +0300

    Program.cs
    Oyuncu için 3 farklı vuruş türünde oran eklendi.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace siraTabanliOyun
+namespace siraTabanliOyun
 {
     public class Program 
     {
@@ -39,40 +39,83 @@
                             // EN - Type of Attack 
                             switch (saldiriTuru)
                             {
-
                                 // TR - Hafif Saldırı
                                 // EN - Light Attack
                                 case 1:
-                                Random saldiri1 = new Random();
-                                int oyuncuHafifSaldiri = saldiri1.Next(0,15);
-                                enemyHP -= oyuncuHafifSaldiri;
+                                // TR - Saldırı Oranı
+                                // EN - Attack Percentage
+                                Random saldiriOrani1 = new Random();
+                                int hafifSaldiriOrani = saldiriOrani1.Next(0,101);
 
-                                Console.WriteLine($"Düşmana {oyuncuHafifSaldiri} değerinde hasar verildi.");
-                                Console.WriteLine($"Oyuncu Can Puanı : {playerHP}\nDüşman Can Puanı : {enemyHP}"); 
+                                    if (hafifSaldiriOrani >= 15 || hafifSaldiriOrani == 100)
+                                    {
+                                        Console.WriteLine($"Hafif saldırı Gerçekleşiyor...");
+                                        Thread.Sleep(1000);
+
+                                        Random saldiri1 = new Random();
+                                        int oyuncuHafifSaldiri = saldiri1.Next(0,15);
+                                        enemyHP -= oyuncuHafifSaldiri;
+
+                                        Console.WriteLine($"Düşmana {oyuncuHafifSaldiri} değerinde hasar verildi.");
+                                        Console.WriteLine($"Oyuncu Can Puanı : {playerHP}\nDüşman Can Puanı : {enemyHP}");
+                                    }
+                                    else 
+                                    {
+                                        Console.WriteLine("Saldırı başarısız.");
+                                    }                                
                                 break;
                                 
                                 // TR - Normal Saldırı
                                 // EN - Normal Attack
                                 case 2:
-                                Random saldiri2 = new Random();
-                                int oyuncuNormalSaldiri = saldiri2.Next(10,25);
-                                enemyHP -= oyuncuNormalSaldiri;
+                                // TR - Saldırı Oranı
+                                // EN - Attack Percentage
+                                Random saldiriOrani2 = new Random();
+                                int normalSaldiriOrani = saldiriOrani2.Next(0,101);
 
-                                Console.WriteLine($"Düşmana {oyuncuNormalSaldiri} değerinde hasar verildi.");
-                                Console.WriteLine($"Oyuncu Can Puanı : {playerHP}\nDüşman Can Puanı : {enemyHP}"); 
+                                    if (normalSaldiriOrani >= 50 || normalSaldiriOrani == 100)
+                                    {
+                                        Console.WriteLine($"Normal saldırı Gerçekleşiyor...");
+                                        Thread.Sleep(2000);
+
+                                        Random saldiri2 = new Random();
+                                        int oyuncuHafifSaldiri = saldiri2.Next(10,25);
+                                        enemyHP -= oyuncuHafifSaldiri;
+
+                                        Console.WriteLine($"Düşmana {oyuncuHafifSaldiri} değerinde hasar verildi.");
+                                        Console.WriteLine($"Oyuncu Can Puanı : {playerHP}\nDüşman Can Puanı : {enemyHP}");
+                                    }
+                                    else 
+                                    {
+                                        Console.WriteLine("Saldırı başarısız.");
+                                    }        
                                 break;
-
                                 
                                 // TR - Ağır Saldırı
                                 // EN - Heavy Attack
                                 case 3:
-                                Random saldiri3 = new Random();
-                                int oyuncuAgirSaldiri = saldiri3.Next(20,35);
-                                enemyHP -= oyuncuAgirSaldiri;
+                                // TR - Saldırı Oranı
+                                // EN - Attack Percentage
+                                Random saldiriOrani3 = new Random();
+                                int agirSaldiriOrani = saldiriOrani3.Next(0,101);
 
-                                Console.WriteLine($"Düşmana {oyuncuAgirSaldiri} değerinde hasar verildi.");
-                                Console.WriteLine($"Oyuncu Can Puanı : {playerHP}\nDüşman Can Puanı : {enemyHP}");
-                                break;
+                                    if (agirSaldiriOrani >= 85 || agirSaldiriOrani == 100)
+                                    {
+                                        Console.WriteLine($"Ağır saldırı Gerçekleşiyor...");
+                                        Thread.Sleep(3500);
+
+                                        Random saldiri2 = new Random();
+                                        int oyuncuHafifSaldiri = saldiri2.Next(20,35);
+                                        enemyHP -= oyuncuHafifSaldiri;
+
+                                        Console.WriteLine($"Düşmana {oyuncuHafifSaldiri} değerinde hasar verildi.");
+                                        Console.WriteLine($"Oyuncu Can Puanı : {playerHP}\nDüşman Can Puanı : {enemyHP}");
+                                    }
+                                    else 
+                                    {
+                                        Console.WriteLine("Saldırı başarısız.");
+                                    }
+                                break;    
                             }
 
                             // TR - Düşman Saldırısı
@@ -84,7 +127,7 @@
                             int dusmanSaldiri1 = dusmanSaldiri.Next(0,20);
                             playerHP -= dusmanSaldiri1;
 
-                            Console.WriteLine($"Düşman {dusmanSaldiri} değerinde hasar verdi.");
+                            Console.WriteLine($"Düşman {dusmanSaldiri1} değerinde hasar verdi.");
                             Console.WriteLine($"Oyuncu Can Puanı : {playerHP}\nDüşman Can Puanı : {enemyHP}");
                         }
 
@@ -95,8 +138,25 @@
                         Random iyilesme = new Random();
                         int oyuncuIyilesme = iyilesme.Next(5,10);
                         playerHP += oyuncuIyilesme;
-                        Console.WriteLine($"{oyuncuIyilesme} değerinde Can Puanı artırılmıştır.");
+
+                       if (playerHP > 100)
+                       {
+                        playerHP = 100;
+
+                         if (playerHP == 100)
+                            {
+                                Random dusmanSaldiri = new Random();
+                                int dusmanSaldiri1 = dusmanSaldiri.Next(5,10);
+                                playerHP -= dusmanSaldiri1;
+
+                                Console.WriteLine($"{oyuncuIyilesme} değerinde Can Puanı artırılmıştır.");
+                                Console.WriteLine($"Düşman {dusmanSaldiri1} değerinde hasar verdi.");
+                            }
+
+                       }
+
                         Console.WriteLine($"Oyuncu Can Puanı : {playerHP}\nDüşman Can Puanı : {enemyHP}");
+                        /*
 
                         // TR - Oyuncunun Can Puanını 100 değerinde sabitleme
                         // EN - Make sure stable Player's HP at 100 value
@@ -107,6 +167,7 @@
                             Console.WriteLine($"Can Puanınız {playerHP}");
                         }
 
+                        */
                     }
                     else 
                     {
